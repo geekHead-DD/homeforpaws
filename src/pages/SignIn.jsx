ad105f497239269fb90906dbb27b384472b68d5a
Debarghya Datta <ujan.dattaa@gmail.com>
2022-08-25 12:41:29 +0530

    start

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-
+import { useState } from 'react'
+// import { toast } from 'react-toastify'
+import { Link, useNavigate } from 'react-router-dom'
+// import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+// import OAuth from '../components/OAuth'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 function SignIn() {
   return (
     <h1>SignIn</h1>
